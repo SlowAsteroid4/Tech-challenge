3bc92e7077d7bfa2a3499ef5fcdb234951cb96fd
Saul <sullivan_erner@hotmail.com>
2021-08-22 20:41:22 -0500

    Lanzamiento de dados con eleccion de caras

--- assets/js/num.al.js
+++ assets/js/num.al.js
@@ -2,12 +2,25 @@ $('#FormAleatorio').bind("submit", function() {
     event.preventDefault();
 
     var NumMin = parseInt($('#MinInput').val());
-    var NumMax = parteInt($('#MaxInput').val());
+    var NumMax = parseInt($('#MaxInput').val());
 
-    if (isNaN(NumMin) || isNaN(NumMax)) {
-        console.log("No es un número");
+    if (isNaN(NumMax)) {
+
+        var dado1 = getNumRand(1, 6);
+        var dado2 = getNumRand(1, 6);
+        $('#ImgDado1').attr("src", "../assets/img/" + dado1 + ".png");
+        $('#ImgDado2').attr("src", "../assets/img/" + dado2 + ".png");
+
+        var SumaDDados = dado1 + dado2;
+        $('#SumaDados').html(SumaDDados);
     } else {
-        console.log(NumMin);
-        console.log(NumMax);
+        var NumGenerado = getNumRand(1, NumMax + 1);
+        var NumGenerado2 = getNumRand(1, NumMax + 1);
+
+        $('#NumGen').html(NumGenerado);
+        $('#NumGen2').html(NumGenerado2);
+
+        var SumaDDados = NumGenerado + NumGenerado2;
+        $('#SumaDados').html(SumaDDados);
     }
 });
