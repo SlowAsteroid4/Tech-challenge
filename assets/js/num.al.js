0151e8362f331b1bd00906b502a7b86bf2c1586f
Saul <sullivan_erner@hotmail.com>
2021-08-23 18:47:07 -0500

    Final

--- assets/js/num.al.js
+++ assets/js/num.al.js
@@ -1,26 +1,117 @@
+//Variables globales
+var NumAnt1 = 0;
+var NumAnt2 = 0;
+
+var NumAnt3 = 0;
+var NumAnt4 = 0;
+
 $('#FormAleatorio').bind("submit", function() {
+
     event.preventDefault();
 
-    var NumMin = parseInt($('#MinInput').val());
-    var NumMax = parseInt($('#MaxInput').val());
+    var caras = parseInt($('#MaxInput').val());
+    var SinRepetir = $('#SinRepetir').prop('checked');
 
-    if (isNaN(NumMax)) {
+    //Dado default 6 caras
+    if (isNaN(caras)) {
 
-        var dado1 = getNumRand(1, 6);
-        var dado2 = getNumRand(1, 6);
-        $('#ImgDado1').attr("src", "../assets/img/" + dado1 + ".png");
-        $('#ImgDado2').attr("src", "../assets/img/" + dado2 + ".png");
+        //Sin repetir
+        if (SinRepetir === true) {
+            var dado1 = getNumRand(1, 7);
+            var dado2 = getNumRand(1, 7);
 
-        var SumaDDados = dado1 + dado2;
-        $('#SumaDados').html(SumaDDados);
-    } else {
-        var NumGenerado = getNumRand(1, NumMax + 1);
-        var NumGenerado2 = getNumRand(1, NumMax + 1);
+            //Si sale un número repetido
+            if (NumAnt1 === dado1 || NumAnt2 === dado2) {
+                while (NumAnt1 === dado1) {
+                    var dado1 = getNumRand(1, 7);
+                }
+                while (NumAnt2 === dado2) {
+                    var dado2 = getNumRand(1, 7);
+                }
 
-        $('#NumGen').html(NumGenerado);
-        $('#NumGen2').html(NumGenerado2);
+                $('#ImgDado1').attr("src", "../assets/img/" + dado1 + ".png");
+                $('#ImgDado2').attr("src", "../assets/img/" + dado2 + ".png");
 
-        var SumaDDados = NumGenerado + NumGenerado2;
-        $('#SumaDados').html(SumaDDados);
+                var SumaDDados = dado1 + dado2;
+                $('#SumaDados').html(SumaDDados);
+
+                NumAnt1 = dado1;
+                NumAnt2 = dado2;
+            }
+
+            //Si no sale un número repetido
+            else {
+                $('#ImgDado1').attr("src", "../assets/img/" + dado1 + ".png");
+                $('#ImgDado2').attr("src", "../assets/img/" + dado2 + ".png");
+
+                var SumaDDados = dado1 + dado2;
+                $('#SumaDados').html(SumaDDados);
+                NumAnt1 = dado1;
+                NumAnt2 = dado2;
+            }
+
+        }
+
+        //Con repetidos
+        else {
+            var dado1 = getNumRand(1, 7);
+            var dado2 = getNumRand(1, 7);
+
+            $('#ImgDado1').attr("src", "../assets/img/" + dado1 + ".png");
+            $('#ImgDado2').attr("src", "../assets/img/" + dado2 + ".png");
+
+            var SumaDDados = dado1 + dado2;
+            $('#SumaDados').html(SumaDDados);
+        }
+    }
+
+    //Elección de cantidad de Caras
+    else {
+
+        var dado1 = getNumRand(1, caras + 1);
+        var dado2 = getNumRand(1, caras + 1);
+
+        //Sin repetir
+        if (SinRepetir === true) {
+
+            //Si sale un número repetido
+            if (NumAnt3 === dado1 || NumAnt4 === dado2) {
+                while (NumAnt3 === dado1) {
+                    var dado1 = getNumRand(1, caras + 1);
+                }
+                while (NumAnt4 === dado2) {
+                    var dado2 = getNumRand(1, caras + 1);
+                }
+
+                $('#NumGen').html(dado1);
+                $('#NumGen2').html(dado2);
+
+                var SumaDDados = dado1 + dado2;
+                $('#SumaDados').html(SumaDDados);
+                NumAnt3 = dado1;
+                NumAnt4 = dado2;
+            }
+
+            //Si no sale un número repetido
+            else {
+                $('#NumGen').html(dado1);
+                $('#NumGen2').html(dado2);
+
+                var SumaDDados = dado1 + dado2;
+                $('#SumaDados').html(SumaDDados);
+                NumAnt3 = dado1;
+                NumAnt4 = dado2;
+            }
+
+        }
+
+        //Con repetidos
+        else {
+            $('#NumGen').html(dado1);
+            $('#NumGen2').html(dado2);
+
+            var SumaDDados = dado1 + dado2;
+            $('#SumaDados').html(SumaDDados);
+        }
     }
 });
